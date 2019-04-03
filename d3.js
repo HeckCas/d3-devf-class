2a23f50abce50477bb7862ea09729f442aff8a98
Ivan Castelan <gho0thubun@gmail.com>
2019-04-03 12:00:08 -0600

    Grafica Interectiva

--- d3.js
+++ d3.js
@@ -22,24 +22,6 @@
     //     if( i == 0 ) { return (Math.random() * 220)+'px' }
     //   })
 
-
-
-
-                     ///Ejercicio 1 ///
-    // const p2 = d3.selectAll('#example2 p');
-    // p2.style('font-size', d => {
-    //   return (Math.random() * 40) + 'px';
-    // });
-    // p2.style('background-color', d => {
-    //   return `rgb(${ Math.random() * 80 }, ${ Math.random() * 80 }, ${ Math.random() * 80  }) `;
-    // });
-    // p2.style('color', (d,i) => {
-    //   if ( i === 0 ) { return 'orange' }
-    // });
-    // p2.attr('class', (d,i) => {
-    //   if ( i === 1 ) { return 'circle' }
-    // });
-
                             /// Ejemplo 3  ///
     // const data = ['Parrafo A', 'Párrafo B', 'Párrafo C', 'Párrafo D'];
     // d3.select('#example3')
@@ -72,21 +54,6 @@
     //   .exit()
     //   .remove();
 
-                        /// Ejercicio 2 ///
-    // const datos = [30, 40, 50, 80, 100];
-    // d3.selectAll('#exercise2 *').remove();
-    // d3.select('#exercise2').selectAll('div')
-    //   .data(datos) // Se hace el enlace con los datos
-    //   .enter()
-    //   .append('p')
-    //   .attr('class', function hola(d, i) {
-    //     if ( i % 2 === 0 ) {
-    //       return 'circle';
-    //     } else {
-    //       return 'square';
-    //     }
-    //   });
-
       // Usar la subselección .enter()
       // Agregar un nuevo div usando .append('div')
 
@@ -102,15 +69,3 @@
       //   .style('margin', '5px')
       //   .style('width', d => d + 'px')
       //   .style('height', '30px');
-
-                      /// Ejercicio 3 ///
-    // const datos = [30, 40, 50, 80, 100];
-    // d3.selectAll('#exercise3 *').remove();
-    // d3.select('#exercise3').selectAll('div')
-    //   .data(datos) // Se hace el enlace con los datos
-    //   .enter() // Regresa todos los datos 'nuevos'
-    //   .append('div')// Agregar un nuevo div para cada dato
-    //   .attr('class', 'circle')
-    //   // A cada circulo dale un width y un height acorde con el valor del dato
-    //   .style('width', d => d + 'px')
-    //   .style('height', d => d + 'px');
